08b14f88e283a343f038ae2004c6fdc074b0de3d
Zerthox <zerthox@gmail.com>
2023-02-21 19:55:18 +0100

    Add known map types

--- src/linked_mem.rs
+++ src/linked_mem.rs
@@ -83,7 +83,7 @@ pub struct Context {
 
     /// Type of the current map.
     ///
-    /// For example `4` for instances.
+    /// See the [`map_type`] module for some known types.
     pub map_type: u32,
 
     /// Current shard id.
@@ -270,4 +270,51 @@ pub enum UIScaling {
     Normal = 1,
     Large = 2,
     Larger = 3,
+}
+
+/// Map type constants.
+///
+/// For example [`map_type::INSTANCE`](self::INSTANCE) holds the map type id for instances.
+pub mod map_type {
+    pub const AUTO_REDIRECT: u32 = 0;
+
+    pub const CHARACTER_CREATION: u32 = 1;
+
+    pub const PVP: u32 = 2;
+
+    pub const GVG: u32 = 3;
+
+    pub const INSTANCE: u32 = 4;
+
+    pub const PVE: u32 = 5;
+
+    pub const TOURNAMENT: u32 = 6;
+
+    pub const TUTORIAL: u32 = 7;
+
+    pub const USER_TOURNAMENT: u32 = 8;
+
+    pub const WVW_ETERNAL_BATTLEGROUNDS: u32 = 9;
+
+    pub const WVW_BLUE_BORDERLANDS: u32 = 10;
+
+    pub const WVW_GREEN_BORDERLANDS: u32 = 11;
+
+    pub const WVW_RED_BORDERLANDS: u32 = 12;
+
+    /// Fortune's Vale.
+    pub const WVW_REWARD: u32 = 13;
+
+    pub const WVW_OBSIDIAN_SANCTUM: u32 = 14;
+
+    pub const WVW_EDGE_OF_THE_MISTS: u32 = 15;
+
+    /// Mini maps like Mistlock Sanctuary, Aerodrome, etc.
+    pub const PVE_MINI: u32 = 16;
+
+    /// PvP 15v15.
+    pub const BIG_BATTLE: u32 = 17;
+
+    /// Armistice Bastion.
+    pub const WVW_LOUNGE: u32 = 18;
 }
